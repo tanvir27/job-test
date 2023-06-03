582e78d51e863c43845c4d7e28e07103a58b20b7
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-06-03 16:32:58 +0600

    adding

--- src/components/ShowProduct/ShowProduct.jsx
+++ src/components/ShowProduct/ShowProduct.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SingleProduct from "../SingleProduct/SingleProduct";
+import { useForm } from "react-hook-form";
 
 const ShowProduct = () => {
+ 
+    
   const [product, setProduct] = useState([]);
 
   useEffect(() => {
@@ -14,16 +17,14 @@ const ShowProduct = () => {
       });
   }, []);
 
-    //limit
-    fetch("https://fakestoreapi.com/products?limit=5")
-      .then((res) => res.json()) 
-        .then((data) => {
-            // console.log(data);
-            setProduct(data)
-        });
-    
+  //limit
+  fetch("https://fakestoreapi.com/products?limit=5")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      setProduct(data);
+    });
 
-    
   const handleSearch = () => {};
 
   return (
@@ -42,17 +43,17 @@ const ShowProduct = () => {
       <div className="d-flex gap-4">
         <div className="col-md-7 cols-sm-12">
           {product.map((single_product) => (
-        <SingleProduct  single_product ={single_product} ></SingleProduct>
+            <SingleProduct single_product={single_product}></SingleProduct>
           ))}
         </div>
 
         <div className="col-md-5 col-sm-12">
-                  <h2>Add New Product</h2>
-                  <form >
-                      
-                      <input type="text" placeholder="Name"/>
-
-                  </form>
+          <h2>Add New Product</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Product Name"/>
+            <input type="text"  Description/>
+            <input type="text" />
+          </form>
         </div>
       </div>
     </div>
